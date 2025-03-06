d9142723f6ad69b489bffc06257c8c0c9344c1f8
Loke <U2507983@rts-365.dk>
2025-03-06 08:58:04 +0100

    glemte at aflevere

--- js/components.js
+++ js/components.js
@@ -1,15 +1,5 @@
 
-function pokemonAboutFormat(num) {
-    let formattedNum = num / 10;
-    return formattedNum.toFixed(1).replace(".", ",");
-}
-
-
-function calcPercentage(a, b) {
-    return Math.floor((a / b) * 100);
-}
-
-function calcNums(num) {
+function formatNums(num) {
     let formatter = new Intl.NumberFormat('en-US', {
         minimumIntegerDigits: 3,
         useGrouping: false
@@ -17,30 +7,33 @@ function calcNums(num) {
     return formatter.format(num);
 }
 
-function setType(value, name) {
+
+function calcPercentage(a, b) {
+    return Math.floor((a / b) * 100);
+}
+
+
+function formatPokemonInfo(num) {
+    let formattedNum = num / 10;
+    return formattedNum.toFixed(1).replace(".", ",");
+}
+
+
+function setPokemonType(value, name) {
     let r = document.documentElement;
     let currentHex = getComputedStyle(r).getPropertyValue(`--${value}`);
 
     r.style.setProperty(`--${name}`, currentHex);
-    r.style.setProperty(`--${name}-bg`, `${currentHex}33`);
+    if (name === "primary-type") {
+        r.style.setProperty(`--${name}-bg`, `${currentHex}33`);
+    }
 }
 
 
-// function findType(value, index) {
-//     let r = document.documentElement;
-//     let currentHex = getComputedStyle(r).getPropertyValue(`--${value}`);
-
-//     r.style.setProperty(`--pokemon-type${index}`, currentHex);
-
-//     return currentHex;
-// }
-
-
-
-
-
-// Get the latest flavor text (in English)
-//let lastIndex = species.flavor_text_entries.map(s => s.language.name === "en").lastIndexOf(true);
-//let mostToDateText = species.flavor_text_entries[lastIndex].flavor_text;
-// Add the flavor text to the pokemon object (optional, but convenient)
-//pokemon.flavorText = mostToDateText;
+function showHide(elm) {
+    if (elm.classList.contains("hidden")) {
+        elm.classList.remove("hidden")
+    } else {
+        elm.classList.add("hidden")
+    }
+}
--- js/details.js
+++ js/details.js
@@ -13,10 +13,10 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${urlName}/`)
         let pokemonName = data.name;
 
         let primaryType = data.types[0].type.name;
-        setType(primaryType, "primary-type");
+        setPokemonType(primaryType, "primary-type");
         if (data.types.length > 1) {
             let secondaryType = data.types[1].type.name;
-            setType(secondaryType, "secondary-type");
+            setPokemonType(secondaryType, "secondary-type");
         }
 
 
@@ -25,21 +25,60 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${urlName}/`)
 
         headerElm.innerHTML = `
             <nav>
-                <i class="fa-solid fa-arrow-left arrow__back"></i>
+                <a href="index.html"><i class="fa-solid fa-arrow-left arrow__back"></i></a>
                 <h1 class="pokemon__name">${pokemonName}</h1>
-                <h2 class="pokemon__number">#${calcNums(id)}</h2>
+                <h2 class="pokemon__number">#${formatNums(id)}</h2>
             </nav>
 
             <div class="pokemon__gallery__wrapper">
                 <figure class="pokemon__gallery">
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="" class="pokemon__gallery__img">
-                    <i class="fa-solid fa-chevron-left pokemon__gallery__arrow arrow__left"></i>
-                    <i class="fa-solid fa-chevron-right pokemon__gallery__arrow arrow__right"></i>
+                    <!-- <i class="fa-solid fa-chevron-left pokemon__gallery__arrow arrow__left"></i>
+                    <i class="fa-solid fa-chevron-right pokemon__gallery__arrow arrow__right"></i> -->
                 </figure>
             </div>
 
             <img src="svg/pokeball.svg" alt="" class="header__bg">
         `;
+
+
+        let pokemonGallery = headerElm.querySelector(".pokemon__gallery");
+
+        let arrowLeft = document.createElement("i");
+        arrowLeft.className = "fa-solid fa-chevron-left pokemon__gallery__arrow arrow__left";
+
+        let arrowRight = document.createElement("i");
+        arrowRight.className = "fa-solid fa-chevron-right pokemon__gallery__arrow arrow__right";
+
+        if (id < 2) {
+            pokemonGallery.append(arrowRight);
+        } else if (id > 1024) {
+            pokemonGallery.append(arrowLeft);
+        } else {
+            pokemonGallery.append(arrowLeft, arrowRight);
+        }
+
+
+        let galleryArrows = document.querySelectorAll(".pokemon__gallery__arrow");
+        galleryArrows.forEach(arrow => {
+            arrow.addEventListener("click", switchPage);
+        });
+
+        function switchPage() {
+            if (this.classList.contains("arrow__left")) {
+                id--;
+            }
+            if (this.classList.contains("arrow__right")) {
+                id++;
+            }
+            window.location.search = "?name=" + id;
+
+            // fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+            //     .then(response => response.json())
+            //     .then(data => {
+            //         window.location.search = "?name=" + data.name;
+            //     })
+        }
 
 
 
@@ -50,10 +89,10 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${urlName}/`)
 
         pokemonTypeContainer.innerHTML = data.types.map((type, index) => {
             let pokemonType = type.type.name;
-            console.log(pokemonType, index + 1);
+            let pokemonTypePriority = ["primary", "secondary"];
 
             return `
-                <span class="pokemon__type pokemon__type__${index + 1}">${pokemonType}</span>
+                <span class="pokemon__type pokemon__type__${pokemonTypePriority[index]}">${pokemonType}</span>
             `;
         }).join("");
 
@@ -72,7 +111,7 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${urlName}/`)
                 <article class="info">
                     <div class="info__text__wrapper info__text__wrapper--weight">
                         <span class="info__text">
-                            <i class="fa-solid fa-weight-hanging info__icon"></i> ${pokemonAboutFormat(data.weight)} kg
+                            <i class="fa-solid fa-weight-hanging info__icon"></i> ${formatPokemonInfo(data.weight)} kg
                         </span>
                     </div>
                     <p class="info__title">Weight</p>
@@ -81,7 +120,7 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${urlName}/`)
                 <article class="info">
                     <div class="info__text__wrapper info__text__wrapper--height">
                         <span class="info__text">
-                            <i class="fa-solid fa-ruler-vertical info__icon"></i> ${pokemonAboutFormat(data.height)} m
+                            <i class="fa-solid fa-ruler-vertical info__icon"></i> ${formatPokemonInfo(data.height)} m
                         </span>
                     </div>
                     <p class="info__title">Height</p>
@@ -117,8 +156,12 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${urlName}/`)
             .then(response => response.json())
             .then(data => {
                 // console.log(data);
+                let dexEntries = data.flavor_text_entries;
+                let dexLatestIndex = dexEntries.map(entry => entry.language.name === "en").lastIndexOf(true);
+                let dexLatest = dexEntries[dexLatestIndex].flavor_text;
+
                 let pokedexDesc = document.querySelector(".about__desc");
-                pokedexDesc.innerHTML = data.flavor_text_entries[9].flavor_text;
+                pokedexDesc.innerHTML = dexLatest;
             });
 
 
@@ -131,7 +174,7 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${urlName}/`)
         let basestatsPercent = [];
 
         data.stats.forEach(stat => {
-            basestats.push(calcNums(stat.base_stat));
+            basestats.push(formatNums(stat.base_stat));
             basestatsNum.push(stat.base_stat);
             basestatsPercent.push(calcPercentage(stat.base_stat, 255));
         });
